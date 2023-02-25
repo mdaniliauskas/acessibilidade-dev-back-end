23d4667f39fea3b2065d0cf920f3f9a9278015c4
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2023-02-24 23:25:23 -0300

    feat: Adicionando o cors para permitir requisições de outros domínios

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require ('dotenv').config({path: './src/configs/.env'});
-
+var cors = require('cors')
 const {
     PORT
 } = process.env
 
 const app = require('./app');
+
+app.use(cors())
 
 app.set('port', PORT || 3000);
 const server = app.listen(app.get('port'), ()=> {
