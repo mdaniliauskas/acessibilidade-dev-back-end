f00d4f58d5805bac1d33c37c74fb5a4e458f3039
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2023-03-06 22:20:39 -0300

    fix: Corrigindo a ordem de execução do cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userRoutes = require('./routes/user.router');
-
+var cors = require('cors')
 const app = express();
+
+app.use(cors())
 
 app.use(userRoutes);
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 require ('dotenv').config({path: './src/configs/.env'});
-var cors = require('cors')
+
 const {
     PORT
 } = process.env
 
 const app = require('./app');
-
-app.use(cors())
 
 app.set('port', PORT || 3000);
 const server = app.listen(app.get('port'), ()=> {
