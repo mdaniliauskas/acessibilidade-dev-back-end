81ac4b32bbbd05370e1b4f7eb55114a4a607fa20
Arthur Nascimento <arthur.nascimento2020@outlook.com>
2023-03-30 14:44:58 -0300

    refactor: resposta e status de erro padronizados nos controllers de news e topic

--- src/controllers/topic.controller.js
+++ src/controllers/topic.controller.js
@@ -1,28 +1,23 @@
 const { Topic } = require("../models/topic");
+const { responseError } = require("../controllers/responseError");
 
 exports.publish = async (req, res) => {
   const objTopic = new Topic(req.body);
   const returnCreate = await objTopic.publishText();
-
   if (returnCreate.id) {
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       message: returnCreate,
     });
-  } else if (returnCreate.code === "P2000") {
+  }
+    
+  try {
+    responseError[returnCreate.code](res)
+  }
+  catch (error) {
     res.status(400).json({
       success: false,
-      message: "The provided value for the column is too long for the column's type.",
-    });
-  } else if (returnCreate.code === "P2003") {
-    res.status(400).json({
-      success: false,
-      message: `Foreign key constraint failed on the field: authorId`,
-    });
-  } else {
-    res.status(500).json({
-      success: false,
-      message: returnCreate,
+      message: "Incorrect field type provided in the JSON input",
     });
   }
 };
@@ -31,39 +26,27 @@ exports.getTopic = async (req, res) => {
   const objTopic = new Topic(req.body);
   const returnConsult = await objTopic.consultText(req.params);
 
-  if (returnConsult) {
-    res.status(200).json({
-      success: true,
-      message: returnConsult,
-    });
-  } else if (returnConsult === null) {
-    res.status(404).json({
+  if (returnConsult === null) {
+    return res.status(404).json({
       success: false,
       message: "Topic not found",
     });
-  }  else {
-    res.status(500).json({
-      success: false,
-      message: returnConsult,
-    });
   }
+  res.status(200).json({
+    success: true,
+    message: returnConsult,
+  });
+  
 };
 
 exports.getAll = async (req, res) => {
   const objTopic = new Topic(req.body);
   const returnList = await objTopic.listTexts();
 
-  if (returnList) {
-    res.status(200).json({
-      success: true,
-      message: returnList,
-    });
-  } else {
-    res.status(500).json({
-      success: false,
-      message: returnList,
-    });
-  }
+  res.status(200).json({
+    success: true,
+    message: returnList,
+  });
 };
 
 exports.update = async (req, res) => {
@@ -71,29 +54,19 @@ exports.update = async (req, res) => {
   const returnUpdate = await objTopic.changeText(req.params);
 
   if (returnUpdate.id) {
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       message: returnUpdate,
     });
-  } else if (returnUpdate.code === "P2025") {
-    res.status(404).json({
-      success: false,
-      message: "An operation failed because it depends on one or more records that were required but not found",
-    });
-  } else if (returnUpdate.code === "P2000") {
+  } 
+
+  try {
+    responseError[returnUpdate.code](res)
+  }
+  catch (error) {
     res.status(400).json({
       success: false,
-      message: "The provided value for the column is too long for the column's type.",
-    });
-  } else if (returnUpdate.code === "P2003") {
-    res.status(400).json({
-      success: false,
-      message: `Foreign key constraint failed on the field`,
-    });
-  } else {
-    res.status(500).json({
-      success: false,
-      message: returnUpdate,
+      message: "Incorrect field type provided in the JSON input",
     });
   }
 };
@@ -103,19 +76,18 @@ exports.remove = async (req, res) => {
   const returnRemove = await objTopic.deleteText(req.params);
 
   if (returnRemove.id) {
-    res.status(200).json({
+    return res.status(200).json({
       success: true,
       message: returnRemove,
     });
-  } else if (returnRemove.code === "P2025") {
-    res.status(404).json({
+  } 
+  try {
+    responseError[returnRemove.code](res)
+  }
+  catch (error) {
+    res.status(400).json({
       success: false,
-      message: "An operation failed because it depends on one or more records that were required but not found",
-    });
-  } else {
-    res.status(500).json({
-      success: false,
-      message: returnRemove,
+      message: "Incorrect field type provided in the JSON input",
     });
   }
 };
